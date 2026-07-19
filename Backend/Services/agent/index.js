c37e1ee8ffa8b84f18eb6c942a965ead31f67946
Milan <milanmekanna@gmail.com>
2026-07-20 03:23:56 +0530

    adding routes of agents in gateway

---
+++ Backend/Services/agent/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js';
+ 
+
+dotenv.config();
+
+const Port = process.env.PORT
+
+const app = express();
+app.use(express.json());
+ 
+
+app.listen(Port,()=>{
+    console.log(`Agent listen on Port ${Port}`);
+    connectDB()
+})
